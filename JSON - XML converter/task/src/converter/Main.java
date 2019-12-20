cbb0549b651320d3b92fc173dbee5bb22516f41f
Balazs <vargyas.b@gmail.com>
2019-12-20 18:14:38 +0100

    Code after Phase1 very shitty implementation

--- JSON - XML converter/task/src/converter/Main.java
+++ JSON - XML converter/task/src/converter/Main.java
@@ -1,8 +1,21 @@
 package converter;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("T");
+        Scanner reader = new Scanner(System.in);
+        Converter converter;
+
+        String input = reader.nextLine().trim();
+
+        if (input.charAt(0) == '<') {
+            converter = new XmlToJsonConverter();
+            System.out.println(converter.convert(input));
+        } else if (input.charAt(0) == '{') {
+            converter = new JsonToXmlConverter();
+            System.out.println(converter.convert(input));
+        }
+
     }
 }
